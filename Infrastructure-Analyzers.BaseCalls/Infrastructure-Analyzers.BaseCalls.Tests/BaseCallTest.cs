31b76cebfa8dcf6d7accb371fb59567e73f7aa4a
Lukas Hartinger <lukas.hartinger@rubicon.eu>
2024-06-27 09:52:04 +0200

    Add Testcase for no diagnostic

--- Infrastructure-Analyzers.BaseCalls/Infrastructure-Analyzers.BaseCalls.Tests/BaseCallTest.cs
+++ Infrastructure-Analyzers.BaseCalls/Infrastructure-Analyzers.BaseCalls.Tests/BaseCallTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.CodeAnalysis.Testing;
+﻿using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Testing;
 using Xunit;
 
 namespace Infrastructure_Analyzers.BaseCalls.Tests;
@@ -6,7 +7,7 @@ namespace Infrastructure_Analyzers.BaseCalls.Tests;
 public class BaseCallTest
 {
     [Fact]
-    public async void BasicBaseCallTest()
+    public async Task NoBaseCall_WithBaseCallMandatory_ReportsDiagnostic()
     {
         const string text = @"
 using Infrastructure_Analyzers.BaseCalls;
@@ -28,11 +29,38 @@ public class DerivedClass : BaseClass
         //base.Test();
     }
 }";
-        /*
+        /* //Uncomment this to get the (hopefully) correct diagnostic to verify against.
          var expected = CSharpAnalyzerVerifier<BaseCallAnalyzer, DefaultVerifier>.Diagnostic()
             .WithLocation(14, 26)
             .WithArguments("Test");
          */
+        var expected = DiagnosticResult.EmptyDiagnosticResults;
+        await CSharpAnalyzerVerifier<BaseCallAnalyzer>.VerifyAnalyzerAsync(text, expected);
+    }
+
+    [Fact]
+    public async Task NoBaseCall_WithBaseCallOptional_ReportsNothing()
+    {
+        const string text = @"
+using Infrastructure_Analyzers.BaseCalls;
+
+public abstract class BaseClass
+{
+    [BaseCallCheck(BaseCall.IsOptional)]
+    public virtual void Test ()
+    {
+        int a = 5;
+    }
+}
+
+public class DerivedClass : BaseClass
+{
+    public override void Test ()
+    {
+        int b = 7;
+        //base.Test();
+    }
+}";
         var expected = DiagnosticResult.EmptyDiagnosticResults;
         await CSharpAnalyzerVerifier<BaseCallAnalyzer>.VerifyAnalyzerAsync(text, expected);
     }
