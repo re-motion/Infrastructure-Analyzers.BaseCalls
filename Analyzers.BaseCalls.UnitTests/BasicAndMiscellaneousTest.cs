1b4f77fb4d1a14a20f609f77f5502ea2dc78461e
michaelludwigrubicon <michael.ludwig@rubicon.eu>
2024-07-30 15:00:51 +0200

    removed redundant imports

--- Analyzers.BaseCalls.UnitTests/BasicAndMiscellaneousTest.cs
+++ Analyzers.BaseCalls.UnitTests/BasicAndMiscellaneousTest.cs
@@ -3,9 +3,7 @@
 
 using System;
 using System.Threading.Tasks;
-using Microsoft.CodeAnalysis.CSharp.Testing;
 using Microsoft.CodeAnalysis.Testing;
-using Microsoft.CodeAnalysis.Testing.Verifiers;
 using Remotion.Infrastructure.Analyzers.BaseCalls.UnitTests.Utilities;
 using Xunit;
 
